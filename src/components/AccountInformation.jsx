9fb83d12b13fac7999e87122e543cd0fe7c89fe5
Markswell <osoromarkswell80@gmail.com>
2023-10-26 11:28:55 +0300

    Adding a GET

--- src/components/AccountInformation.jsx
+++ src/components/AccountInformation.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import TransactionList from './TransactionList'
+import AddTransactions from './AddTransactions' 
 
 function AccountInformation() {
-    
+    const [transaction, setTransaction] = useState([])
+    const [query, setQuery] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
+  function handleSearch(e) {
+    setQuery(e.target.value)
   return (
     <div>
-        {/* <Search handleSearch={handleSearch} />
-        <AddTransactionForm /> */}
+        <Search handleSearch={handleSearch} />
+        <AddTransactions /> 
         <TransactionList transactions={transaction} />
     </div>
   )
+}
 }
 
 export default AccountInformation
